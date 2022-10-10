b19b8ebd5cdbc74cdc19ceec4b0421a491f77023
Gin <115474107+GinKatsumi@users.noreply.github.com>
2022-10-10 10:12:21 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,15 @@
-function compweb(){
-    var Element = document.getElementById("ina");
-    Element.innerHTML = "1. Entrar no site da Lojas Tsar.<br>2. Escolha os produtos que voçê mais gostou.<br>3. Selecione o tamanho e a cor.<br>4. Adicione os produtos ao carrinho.Adicione o endereço para entrega.<br> 5. Selecione o método de pagamento.<br> 6. Finalize a compra.<br> 7. Pronto! Agora é só aguardar seus produtos chegarem.";
+var escolha1 = 0
+var escolha2 = 0
+var escolha3 = 0
+var jogador {
+    vida: 100
+}
+var inimigo1 {
+    vida: 100
+}
+var inimigo2 {
+    vida: 100
+}
+var inimigo3 {
+    vida: 100
 }
