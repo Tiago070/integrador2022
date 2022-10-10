b7a25099a008b2c4046e699963c318a35a408f89
Gin <115474107+GinKatsumi@users.noreply.github.com>
2022-10-10 10:13:23 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
 var escolha1 = 0
 var escolha2 = 0
 var escolha3 = 0
-var jogador {
+var jogador = {
     vida: 100
 }
-var inimigo1 {
+var inimigo1 = {
     vida: 100
 }
-var inimigo2 {
+var inimigo2 = {
     vida: 100
 }
-var inimigo3 {
+var inimigo3 = {
     vida: 100
 }
