f47f1bc598e4dea3ab9a36b63d664a59db0d457e
Tiago070 <115472816+Tiago070@users.noreply.github.com>
2022-10-10 09:48:08 -0300

    Create script.js
    aaaaaaaaaa

---
+++ script.js
@@ -0,0 +1,4 @@
+function compweb(){
+    var Element = document.getElementById("ina");
+    Element.innerHTML = "1. Entrar no site da Lojas Tsar.<br>2. Escolha os produtos que voçê mais gostou.<br>3. Selecione o tamanho e a cor.<br>4. Adicione os produtos ao carrinho.Adicione o endereço para entrega.<br> 5. Selecione o método de pagamento.<br> 6. Finalize a compra.<br> 7. Pronto! Agora é só aguardar seus produtos chegarem.";
+}
