ad14c54dc2da039d3f718a2fa569f7091bf4d2b3
andrewflorence2208 <115472944+andrewflorence2208@users.noreply.github.com>
2022-10-15 10:30:54 -0300

    Update battle-tests.js

--- battle-tests.js
+++ battle-tests.js
@@ -1,12 +1,29 @@
 const char = document.getElementById("char");
 
-const atk = () => {
-    char.classList.add("atk");
+function enemys_life(dmg){
+    var enemys_life_value = parseInt(document.getElementById("enemys_life").getAttribute("value"));
 
-    setTimeout( () => {
-        char.classList.remove("atk")
-    }, 50);
-    console.log("atk")
+    enemys_life_value -= parseInt(dmg);
+
+    document.getElementById("enemys_life").setAttribute("value", enemys_life_value);
+
+    if (enemys_life_value <= 0){
+        window.alert("die")
+    };
+
+    console.log(enemys_life_value);
 };
 
-document.addEventListener("keydown",  atk);
+function atk(){
+
+    //ANIMATION BY CSS CLASS:
+        char.classList.add("atk");
+
+        setTimeout( () => {
+            char.classList.remove("atk")
+        }, 50);
+
+        console.log("atk")
+
+    enemys_life(10)
+};
